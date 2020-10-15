001c470084a55b9a8d35fa4b8e23f53ab7b41826
Henry <cryptix@riseup.net>
2020-10-15 12:07:05 +0200

    more testing

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const flotilla = require("./");
+const pull = require('pull-stream')
 
 const ssb = flotilla();
 
@@ -6,6 +7,14 @@ ssb.whoami((err, res) => {
   if (err) throw err;
   console.log(res.id);
 
-  // Apparently ssb.close() throws errors now?!
-  process.exit(0);
+  pull(
+    ssb.createLogStream({live: false, limit:5, reverse: true}),
+    pull.collect((err, msgs) => {
+      if (err) throw err;
+      console.log(msgs);
+
+      // Apparently ssb.close() throws errors now?!
+      process.exit(0);
+    })
+  )
 });
